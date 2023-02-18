629f63b146c11d9865276b19c85c5239da618379
AnyaLyadova <124922006+AnyaLyadova@users.noreply.github.com>
2023-02-18 19:33:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,18 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    int result = 0;
+	int del = 0;
+	if (a > b) {
+		del = a;
+	}
+	else {
+		del = b;
+	}
+	for (int i = 1; i <= del; ++i) {
+		if ((a % i == 0) && (b % i == 0) && (i > result)) {
+			result = i;
+		}
+	}
+		return result;
 }
